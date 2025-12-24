5518a109599fc9b3bdc3831ae75a1be2ff46e038
Marc Soda <marc@soda.fm>
2025-12-23 22:22:08 -0500

    fixes

--- roles/workstation/files/first-login.sh
+++ roles/workstation/files/first-login.sh
@@ -11,6 +11,13 @@ mkdir -p "$(dirname "$LOCK_FILE")"
 
 ### User actions
 
+# Networking
+sudo systemctl start NetworkManager
+sudo systemctl enable NetworkManager
+sudo systemctl start systemd-resolved
+sudo systemctl enable systemd-resolved
+sudo ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
+
 # Setup the battery monitor if we have one
 if ls /sys/class/power_supply/BAT* &>/dev/null; then
   powerprofilesctl set balanced || true
@@ -19,8 +26,8 @@ else
   powerprofilesctl set performance || true
 fi
 
-# Systemd-resolved
-sudo ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
+# Keyring
+set_default_keyring
 
 # Elephant
 #elephant service enable
@@ -28,3 +35,31 @@ sudo ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
 
 umask 077
 : > "$LOCK_FILE"
+
+sudo systemctl enable sddm
+sudo systemctl start sddm
+
+set_default_keyring() {
+    keyring_dir="$HOME/.local/share/keyrings"
+    keyring_file="$keyring_dir/Default_keyring.keyring"
+    default_file="$keyring_dir/default"
+
+    mkdir -p $keyring_dir
+
+    cat << EOF | tee "$keyring_file"
+[keyring]
+display-name=Default keyring
+ctime=$(date +%s)
+mtime=0
+lock-on-idle=false
+lock-after=false
+EOF
+
+    cat << EOF | tee "$default_file"
+Default_keyring
+EOF
+
+    chmod 700 "$keyring_dir"
+    chmod 600 "$keyring_file"
+    chmod 644 "$default_file"
+}
